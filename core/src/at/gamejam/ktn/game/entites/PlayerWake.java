c84885e8678e478e61297c49c51256a865fab2b8
TheElk205 <Koeppen.Ferdinand@gmail.com>
2015-04-18 17:21:44 +0200

    Player Wake

--- core/src/at/gamejam/ktn/game/entites/PlayerWake.java
+++ core/src/at/gamejam/ktn/game/entites/PlayerWake.java
@@ -1,5 +1,26 @@
 package at.gamejam.ktn.game.entites;
 
-public class PlayerWake {
+import at.gamejam.ktn.game.WorldController;
+import at.gamejam.ktn.game.entites.Player.ItemType;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class PlayerWake extends Player {
+
+	public PlayerWake(final Vector2 position, WorldController world) {
+		this.initConstructor(position, world);
+		this.itemType = ItemType.REDBULL;
+	}
+	
+	@Override
+	protected void loadAsset() {
+		this.texture = this.assets.findRegion("player");
+	}
+
+	@Override
+	public void hitByItem(Item item) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
