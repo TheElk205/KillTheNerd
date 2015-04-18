3a55c5ad997d9ccdddd1396151e6f9989c174401
Herk0r <k.herkt@gmx.at>
2015-04-18 15:49:18 +0200

    Input manager

---
+++ core/src/at/gamejam/ktn/game/InputManager.java
@@ -0,0 +1,130 @@
+package at.gamejam.ktn.game;
+
+import at.gamejam.ktn.game.entites.Player;
+import at.gamejam.ktn.game.entites.PlayerSleep;
+import at.gamejam.ktn.utils.CameraHelper;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputAdapter;
+
+public class InputManager extends InputAdapter {
+
+	private final PlayerSleep	player;
+	private final CameraHelper	cameraHelper;
+
+	public InputManager(final PlayerSleep player, final CameraHelper cameraHelper) {
+		this.player = player;
+		this.cameraHelper = cameraHelper;
+	}
+
+	@Override
+	public boolean keyDown(final int keycode) {
+		switch (keycode) {
+			case Input.Keys.PLUS:
+				this.cameraHelper.addZoom(-0.2f);
+				break;
+			case Input.Keys.MINUS:
+				this.cameraHelper.addZoom(0.2f);
+				break;
+			case Input.Keys.LEFT:
+				this.player.setLeft(true);
+				this.player.setDirectionMoving(Player.Direction.W);
+				break;
+			case Input.Keys.RIGHT:
+				this.player.setRight(true);
+				this.player.setDirectionMoving(Player.Direction.E);
+				break;
+			case Input.Keys.UP:
+				this.player.setUp(true);
+				this.player.setDirectionMoving(Player.Direction.N);
+				break;
+			case Input.Keys.DOWN:
+				this.player.setDown(true);
+				this.player.setDirectionMoving(Player.Direction.S);
+				break;
+			/*case Input.Keys.D:
+			this.debug = !this.debug;
+			break;*/
+			case Input.Keys.SPACE:
+				this.player.throwItem();
+				break;
+			/*case Input.Keys.R:
+			this.reset = true;
+			break;*/
+			default:
+				this.player.stop();
+				break;
+		}
+		return true;
+	}
+
+	@Override
+	public boolean keyUp(final int keycode) {
+		switch (keycode) {
+			case Input.Keys.LEFT:
+				this.player.setLeft(false);
+				this.player.setDirectionMoving(Player.Direction.W);
+				break;
+			case Input.Keys.RIGHT:
+				this.player.setRight(false);
+				this.player.setDirectionMoving(Player.Direction.E);
+				break;
+			case Input.Keys.UP:
+				this.player.setUp(false);
+				this.player.setDirectionMoving(Player.Direction.N);
+				break;
+			case Input.Keys.DOWN:
+				this.player.setDown(false);
+				this.player.setDirectionMoving(Player.Direction.S);
+				break;
+			default:
+				// System.out.println("Player Stop");
+				this.player.stop();
+				break;
+		}
+
+		if (!this.player.getDown() && !this.player.getUp() && !this.player.getLeft() && !this.player.getRight()) {
+			this.player.stop();
+		}
+		/*switch (keycode) {
+			case Input.Keys.LEFT:
+			case Input.Keys.RIGHT:
+			case Input.Keys.UP:
+			case Input.Keys.DOWN:
+				this.player.stop();
+				break;
+			default:
+				this.player.stop();
+				break;
+		}*/
+		return false;
+	}
+
+	/*@Override
+	public boolean touchDown(final int screenX, final int screenY, final int pointer, final int button) {
+		if (screenY < (Gdx.graphics.getHeight() / 4)) {
+			this.reset();
+		}
+		if (screenX < (Gdx.graphics.getWidth() / 3)) {
+			// this.player.setLeft(true);
+		} else
+			if (screenX > ((Gdx.graphics.getWidth() / 3) * 2)) {
+				// this.player.setRight(true);
+			} else {
+				// this.player.jump();
+			}
+		return true;
+	}
+
+	@Override
+	public boolean touchUp(final int screenX, final int screenY, final int pointer, final int button) {
+		if (screenX < (Gdx.graphics.getWidth() / 3)) {
+			// this.player.setLeft(false);
+		} else
+			if (screenX > ((Gdx.graphics.getWidth() / 3) * 2)) {
+				// this.player.setRight(false);
+			}
+		return true;
+	}*/
+
+}
--- core/src/at/gamejam/ktn/game/WorldController.java
+++ core/src/at/gamejam/ktn/game/WorldController.java
@@ -2,26 +2,23 @@ package at.gamejam.ktn.game;
 
 import java.util.Vector;
 
-import at.gamejam.ktn.game.entites.Player;
 import at.gamejam.ktn.game.entites.PlayerSleep;
 import at.gamejam.ktn.game.entites.RedBull;
 import at.gamejam.ktn.game.entities.GameObject;
 import at.gamejam.ktn.utils.CameraHelper;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class WorldController extends InputAdapter {
+public class WorldController {
 	public CameraHelper					cameraHelper;
 	public PlayerSleep					player;
 	public long							timeElapsed;
 	protected int						coinCount	= 0;
 	private World						b2World;
 	private TopDownLevel				level;
-	private boolean						debug		= true;
+	private final boolean				debug		= true;
 	private boolean						reset;
 
 	private final Vector<GameObject>	newObjects	= new Vector<GameObject>();
@@ -31,7 +28,7 @@ public class WorldController extends InputAdapter {
 	}
 
 	public void init() {
-		Gdx.input.setInputProcessor(this);
+
 		this.cameraHelper = new CameraHelper();
 		// this.b2World = new World(new Vector2(0, -9.81f), true);
 		this.b2World = new World(new Vector2(0, 0), true); // no gravity
@@ -40,6 +37,7 @@ public class WorldController extends InputAdapter {
 		// this.level = new Level(this.b2World);
 		this.level = new TopDownLevel(this.b2World);
 		this.b2World.setContactListener(new MyContactListener(this));
+		Gdx.input.setInputProcessor(new InputManager(this.player, this.cameraHelper));
 	}
 
 	public void update(final float deltaTime) {
@@ -71,9 +69,9 @@ public class WorldController extends InputAdapter {
 		}
 		this.player.update(deltaTime);
 		this.level.update(deltaTime);
-		if (this.player.getBody().getPosition().y < -3) {
+		/*if (this.player.getBody().getPosition().y < -3) {
 			this.reset = true;
-		}
+		}*/
 		// this.testCoins();
 	}
 
@@ -101,85 +99,6 @@ public class WorldController extends InputAdapter {
 			red.grabbed(this.player);
 			this.coinCount += 1;
 		}
-	}*/
-
-	@Override
-	public boolean keyDown(final int keycode) {
-		switch (keycode) {
-			case Input.Keys.PLUS:
-				this.cameraHelper.addZoom(-0.2f);
-				break;
-			case Input.Keys.MINUS:
-				this.cameraHelper.addZoom(0.2f);
-				break;
-			case Input.Keys.LEFT:
-				this.player.setDirectionMoving(Player.Direction.W);
-				break;
-			case Input.Keys.RIGHT:
-				this.player.setDirectionMoving(Player.Direction.E);
-				break;
-			case Input.Keys.UP:
-				this.player.setDirectionMoving(Player.Direction.N);
-				break;
-			case Input.Keys.DOWN:
-				this.player.setDirectionMoving(Player.Direction.S);
-				break;
-			case Input.Keys.D:
-				this.debug = !this.debug;
-				break;
-			case Input.Keys.SPACE:
-				this.player.throwItem();
-				break;
-			case Input.Keys.R:
-				this.reset = true;
-				break;
-			default:
-				break;
-		}
-		return true;
-	}
-
-	@Override
-	public boolean keyUp(final int keycode) {
-		switch (keycode) {
-			case Input.Keys.LEFT:
-			case Input.Keys.RIGHT:
-			case Input.Keys.UP:
-			case Input.Keys.DOWN:
-				this.player.stop();
-				break;
-			default:
-				this.player.stop();
-				break;
-		}
-		return false;
-	}
-
-	/*@Override
-	public boolean touchDown(final int screenX, final int screenY, final int pointer, final int button) {
-		if (screenY < (Gdx.graphics.getHeight() / 4)) {
-			this.reset();
-		}
-		if (screenX < (Gdx.graphics.getWidth() / 3)) {
-			// this.player.setLeft(true);
-		} else
-			if (screenX > ((Gdx.graphics.getWidth() / 3) * 2)) {
-				// this.player.setRight(true);
-			} else {
-				// this.player.jump();
-			}
-		return true;
-	}
-
-	@Override
-	public boolean touchUp(final int screenX, final int screenY, final int pointer, final int button) {
-		if (screenX < (Gdx.graphics.getWidth() / 3)) {
-			// this.player.setLeft(false);
-		} else
-			if (screenX > ((Gdx.graphics.getWidth() / 3) * 2)) {
-				// this.player.setRight(false);
-			}
-		return true;
 	}*/
 
 	public void addTempGameObject(final GameObject object) {
