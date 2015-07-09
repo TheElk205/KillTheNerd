b561e8e14e9a35c7180a3e95b5e896329e082175
Hribse <Hribse@Hribse-PC>
2015-07-09 22:21:35 +0200

    qwertz

--- desktop/src/at/game/desktop/DesktopLauncher.java
+++ desktop/src/at/game/desktop/DesktopLauncher.java
@@ -21,7 +21,7 @@ public class DesktopLauncher {
 		if (DesktopLauncher.REBUILD_ALTLAS) {
 			final TexturePacker.Settings settings = new TexturePacker.Settings();
 			settings.edgePadding = true;
-			settings.maxWidth = 1024;asdfasdfasdfdfsfgsdfgsdjffölaljafsdfasdfadfasdfasdfassdfaf
+			settings.maxWidth = 1024;int a = 7;
 			settings.maxHeight = 1024; 
 			settings.debug = false;
 			settings.duplicatePadding = true;
